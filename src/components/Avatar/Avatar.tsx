088800e8f10c4d68bb97053fe5446e1ffc046425
Klynsman <klynsman.lemos@itemm.org.br>
2023-04-05 16:49:39 -0300

    adicionando borda no avatar

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,133 +1,147 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 
-type Variant = 'circular' | 'square' | 'rounded';
-type Size = 'xs' | 'sm' | 'md' | 'xl'
+type Variant = "circular" | "square" | "rounded";
+type Size = "xs" | "sm" | "md" | "xl";
 
 export type AvatarProps = {
-    children: React.ReactNode,
-    style?: React.CSSProperties,
-    variant?: Variant,
-    size?: Size,
-    src?: any,
-    alt?: string,
-    notify?: boolean
-    value?: number
+  children: React.ReactNode;
+  style?: React.CSSProperties;
+  variant?: Variant;
+  size?: Size;
+  src?: any;
+  alt?: string;
+  notify?: boolean;
+  value?: number;
+  border?: boolean;
 };
 
-export const Avatar = (
-    {
-        style,
-        variant,
-        size,
-        src,
-        alt,
-        notify,
-        value
-    }: AvatarProps
-) => {
+// Criar a borda do avatar!
 
-    return(
-        <div className="flex flex-row">
-            {
-                notify === true && variant === 'circular' && (
-                    <div 
-                        className={
-                            size === 'xs' ? '' :
-                            size === 'sm' ? `notify-badge-sm` :
-                            size === 'md' ? `notify-badge-md` :
-                            'notify-badge'
-                        }
-                        >
-                            {
-                                size === 'xs' ? '' :
-                                value !== undefined && value > 99 ? '99+' : value
-                            }
-                    </div>
-                )
-                }
-                {
-                    notify === true && variant === 'square' && (
-                        <div 
-                            className={
-                                size === 'xs' ? '' :
-                                size === 'sm' ? `notify-badge-square-sm` :
-                                size === 'md' ? `notify-badge-square-md` :
-                                size === 'xl' ? `notify-badge-square-xl` :
-                                'notify-badge'
-                            }
-                            >
-                                {
-                                    size === 'xs' ? '' :
-                                    value !== undefined && value > 99 ? '99+' : value
-                                }
-                        </div>
-                    )
-                }
-                {
-                    notify === true && variant === 'rounded' && (
-                        <div 
-                            className={
-                                size === 'xs' ? '' :
-                                size === 'sm' ? `notify-badge-square-sm` :
-                                size === 'md' ? `notify-badge-square-md` :
-                                size === 'xl' ? `notify-badge-square-xl` :
-                                'notify-badge'
-                            }
-                            >
-                                {
-                                    size === 'xs' ? '' :
-                                    value !== undefined && value > 99 ? '99+' : value
-                                }
-                        </div>
-                    )
-                }
-                {
-                    variant === 'circular' ? (
-                        <img 
-                            style={style}
-                            className={
-                                size === size ? `image-circular ${size}`:
-                                `image-circular`
-                            }
-                            src={src}
-                            alt={alt}
-                        />
-                    )
-                    :
-                    variant === 'square' ? (
-                        <img 
-                            style={style}
-                            className={
-                                size === size ? `image-square ${size}`:
-                                `image-square`
-                            }
-                            src={src}
-                            alt={alt}
-                        />
-                    )
-                    :
-                    variant === 'rounded' ? (
-                        <img 
-                            style={style}
-                            className={
-                                size === size ? `image-rounded ${size}`:
-                                `image-rounded`
-                            }
-                            src={src}
-                            alt={alt}
-                        />
-                    )
-                    :
-                    (
-                        <img 
-                            style={style}
-                            className="image-circular"
-                            src={src}
-                            alt={alt}
-                        />
-                    )
-                }
-            </div>
-    )
-}
+export const Avatar = ({
+  style,
+  variant,
+  size,
+  src,
+  alt,
+  notify,
+  value,
+  border,
+}: AvatarProps) => {
+  return (
+    <div className="flex flex-row">
+      {notify === true && variant === "circular" && (
+        <div
+          className={
+            size === "xs"
+              ? ""
+              : size === "sm"
+              ? `notify-badge-sm`
+              : size === "md"
+              ? `notify-badge-md`
+              : "notify-badge"
+          }
+        >
+          {size === "xs"
+            ? ""
+            : value !== undefined && value > 99
+            ? "99+"
+            : value}
+        </div>
+      )}
+      {notify === true && variant === "square" && (
+        <div
+          className={
+            size === "xs"
+              ? ""
+              : size === "sm"
+              ? `notify-badge-square-sm`
+              : size === "md"
+              ? `notify-badge-square-md`
+              : size === "xl"
+              ? `notify-badge-square-xl`
+              : "notify-badge"
+          }
+        >
+          {size === "xs"
+            ? ""
+            : value !== undefined && value > 99
+            ? "99+"
+            : value}
+        </div>
+      )}
+      {notify === true && variant === "rounded" && (
+        <div
+          className={
+            size === "xs"
+              ? ""
+              : size === "sm"
+              ? `notify-badge-square-sm`
+              : size === "md"
+              ? `notify-badge-square-md`
+              : size === "xl"
+              ? `notify-badge-square-xl`
+              : "notify-badge"
+          }
+        >
+          {size === "xs"
+            ? ""
+            : value !== undefined && value > 99
+            ? "99+"
+            : value}
+        </div>
+      )}
+      {variant === "circular" ? (
+        <div className={border === true ? "border-circular" : ""} style={style}>
+          <img
+            style={style}
+            className={
+              size === size ? `image-circular ${size}` : `image-circular `
+            }
+            src={src}
+            alt={alt}
+          />
+        </div>
+      ) : variant === "square" ? (
+        <div className={border === true ? "border-square" : ""} style={style}>
+          <img
+            style={style}
+            className={
+              size === size
+                ? `image-square ${size}`
+                : border === true
+                ? `image-square border`
+                : `image-square`
+            }
+            src={src}
+            alt={alt}
+          />
+        </div>
+      ) : variant === "rounded" ? (
+        <div className={border === true ? "border-rounded" : ""} style={style}>
+          <img
+            style={style}
+            className={
+              size === size
+                ? `image-rounded ${size}`
+                : border === true
+                ? `image-rounded border`
+                : `image-rounded`
+            }
+            src={src}
+            alt={alt}
+          />
+        </div>
+      ) : (
+        <img
+          style={style}
+          className={
+            border === true ? `image-circular border` : `image-circular`
+          }
+          src={src}
+          alt={alt}
+        />
+      )}
+    </div>
+  );
+};
--- src/stories/avatar.stories.tsx
+++ src/stories/avatar.stories.tsx
@@ -1,65 +1,153 @@
-import { Avatar, AvatarProps } from 'src/components/Avatar/Avatar';
-import { Grid } from 'src/components/Grid/Grid';
+import { Avatar, AvatarProps } from "src/components/Avatar/Avatar";
+import { Grid } from "src/components/Grid/Grid";
 
 const meta = {
-    title: "Data Display/Avatar",
-    component: Avatar,
-    tags: ["docsPage"],
+  title: "Data Display/Avatar",
+  component: Avatar,
+  tags: ["docsPage"],
 };
 
 export default meta;
 
-const users = [
-    'https://avatars.githubusercontent.com/u/12592949', 
-    'https://avatars.githubusercontent.com/u/8225666', 
-    'https://avatars.githubusercontent.com/u/15609339', 
-    'https://avatars.githubusercontent.com/u/14308293', 
-    'https://avatars.githubusercontent.com/u/1203827', 
-    'https://avatars.githubusercontent.com/u/9625224', 
-    'https://avatars.githubusercontent.com/u/15884443',
-    'https://avatars.githubusercontent.com/u/10924138', 
-    'https://avatars.githubusercontent.com/u/2797600',
-    'https://avatars.githubusercontent.com/u/23637144'
+const avatar = [
+  {
+    color: "#2196f3",
+    avatar: "https://avatars.githubusercontent.com/u/15609339",
+  },
+  {
+    color: "#f44336",
+    avatar: "https://avatars.githubusercontent.com/u/15609339",
+  },
+  {
+    color: "#4caf50",
+    avatar: "https://avatars.githubusercontent.com/u/15609339",
+  },
+  {
+    color: "#ffb300",
+    avatar: "https://avatars.githubusercontent.com/u/15609339",
+  },
+  {
+    color: "#a6a6a6",
+    avatar: "https://avatars.githubusercontent.com/u/15609339",
+  },
+  {
+    color: "#db8000",
+    avatar: "https://avatars.githubusercontent.com/u/15609339",
+  },
+  {
+    color: "#00BCD4",
+    avatar: "https://avatars.githubusercontent.com/u/15609339",
+  },
+  {
+    color: "#673AB7",
+    avatar: "https://avatars.githubusercontent.com/u/15609339",
+  },
 ];
 
 const TeplateDefault = (args: AvatarProps) => (
-    <Avatar 
-        {...args} 
-        src="https://avatars.githubusercontent.com/u/15609339"
-    />
+  <Avatar {...args} src="https://avatars.githubusercontent.com/u/15609339" />
 );
 
 const TeplateNotification = (args: AvatarProps) => (
-    <>
-        <Avatar 
-            {...args} 
-            src="https://avatars.githubusercontent.com/u/15609339"
-            notify={true}
-            variant="circular"
-            value={5}
-        />
-        <div style={{marginTop: 10}}>
-            <Avatar 
-                {...args} 
-                src="https://avatars.githubusercontent.com/u/15609339"
-                notify={true}
-                variant="square"
-                value={50}
-            />
-        </div>
-        <div style={{marginTop: 10}}>
-            <Avatar 
-                {...args} 
-                src="https://avatars.githubusercontent.com/u/15609339"
-                notify={true}
-                variant="rounded"
-                value={100}
-            />
-        </div>
-    </>
+  <>
+    <Avatar
+      {...args}
+      src="https://avatars.githubusercontent.com/u/15609339"
+      notify={true}
+      variant="circular"
+      value={5}
+    />
+    <div style={{ marginTop: 10 }}>
+      <Avatar
+        {...args}
+        src="https://avatars.githubusercontent.com/u/15609339"
+        notify={true}
+        variant="square"
+        value={50}
+      />
+    </div>
+    <div style={{ marginTop: 10 }}>
+      <Avatar
+        {...args}
+        src="https://avatars.githubusercontent.com/u/15609339"
+        notify={true}
+        variant="rounded"
+        value={100}
+      />
+    </div>
+  </>
 );
 
+const TeplateBorder = (args: AvatarProps) => (
+  <>
+    <Grid spacing={20}>
+      <Avatar
+        {...args}
+        src="https://avatars.githubusercontent.com/u/15609339"
+        variant="circular"
+        border={true}
+      />
+      <Avatar
+        {...args}
+        src="https://avatars.githubusercontent.com/u/15609339"
+        variant="square"
+        border={true}
+      />
+      <Avatar
+        {...args}
+        src="https://avatars.githubusercontent.com/u/15609339"
+        variant="rounded"
+        border={true}
+      />
+    </Grid>
+  </>
+);
 
+const TeplateBorderColor = (args: AvatarProps) => (
+  <>
+    <Grid spacing={20}>
+      {avatar.map((res) => (
+        <Avatar
+          {...args}
+          src={res.avatar}
+          variant="circular"
+          border={true}
+          style={{ borderColor: `${res.color}` }}
+        />
+      ))}
+    </Grid>
+
+    <div style={{ marginTop: 10 }}>
+      <Grid spacing={20}>
+        {avatar.map((res) => (
+          <Avatar
+            {...args}
+            src={res.avatar}
+            variant="rounded"
+            border={true}
+            style={{ borderColor: `${res.color}` }}
+          />
+        ))}
+      </Grid>
+    </div>
+
+    <div style={{ marginTop: 10 }}>
+      <Grid spacing={20}>
+        {avatar.map((res) => (
+          <Avatar
+            {...args}
+            src={res.avatar}
+            variant="square"
+            border={true}
+            style={{ borderColor: `${res.color}` }}
+          />
+        ))}
+      </Grid>
+    </div>
+  </>
+);
 
 export const Default = TeplateDefault.bind({});
 export const notification = TeplateNotification.bind({});
+export const border = TeplateBorder.bind({});
+export const borderColor = TeplateBorderColor.bind({});
